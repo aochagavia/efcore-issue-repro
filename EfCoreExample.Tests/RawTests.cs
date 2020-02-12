0a47866a6d12ad9ab676910d57cdad4740fac4fb
Adolfo Ochagavía <github@adolfo.ochagavia.xyz>
2020-02-12 17:30:00 +0100

    Simplify tests

--- EfCoreExample.Tests/RawTests.cs
+++ EfCoreExample.Tests/RawTests.cs
@@ -40,5 +40,62 @@ namespace EfCoreExample.Tests
                 Assert.Equal("Dummy", registration.Contact.LastName);
             }
         }
+
+        [Fact]
+        public void Modify_Contact_LastName_For_Existing_Contact_IsSaved()
+        {
+            EnsureDbExists();
+
+            Guid? id;
+            using (var db = new Db())
+            {
+                var registration = new Registration();
+                registration.Contact.LastName = "Doe";
+                db.Registrations.Add(registration);
+                db.SaveChanges();
+                id = registration.Id;
+            }
+
+            using (var db = new Db())
+            {
+                var registration = db.Registrations.Find(id);
+                registration.Contact.LastName = "Dummy";
+                db.SaveChanges();
+            }
+
+            using (var db = new Db())
+            {
+                var registration = db.Registrations.Find(id);
+                Assert.Equal("Dummy", registration.Contact.LastName);
+            }
+        }
+
+        [Fact]
+        public void Modify_Contact_LastName_Using_Replacement_IsSaved()
+        {
+            EnsureDbExists();
+
+            Guid? id;
+            using (var db = new Db())
+            {
+                var registration = new Registration();
+                db.Registrations.Add(registration);
+                db.SaveChanges();
+                id = registration.Id;
+            }
+
+            using (var db = new Db())
+            {
+                var registration = db.Registrations.Find(id);
+                registration.Contact = new Contact {LastName = "Dummy"};
+                db.SaveChanges();
+            }
+
+            using (var db = new Db())
+            {
+                var registration = db.Registrations.Find(id);
+                Assert.Equal("Dummy", registration.Contact.LastName);
+            }
+        }
     }
 }
